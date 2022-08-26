89764317a706c4315bf3457246d20281a9a53443
Amol Pawar <amolpawar4334@gmail.com>
2022-08-26 17:45:32 +0530

    Code updated for class

--- src/TestPackage/A.java
+++ src/TestPackage/A.java
@@ -1,10 +1,17 @@
 package TestPackage;
 
-public class A {
+import org.testng.annotations.Test;
 
+public class A {
+@Test 
+void m1()
+{
+	}
 	public static void main(String[] args) {
-		System.out.println("This is at line 6");
-		System.out.println("dsd");
+		//
+		System.out.println(args.length);
+		System.out.println("");
+		System.out.println("");
 	}
 
 }
